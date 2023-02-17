ac49a80e60e0fe1bdcd8a5879e867e4c42e77224
John Giorshev <johngiorshev@gmail.com>
2023-02-17 12:59:04 -0500

    minor edit

--- scripts/choose.bash
+++ scripts/choose.bash
@@ -2,7 +2,7 @@
 hist() {
   local LINE=$(
     builtin fc -lnr -2147483648 |
-    tail -c +3 | head -c -1 | sed -z 's/\n\t./\x0/g' |
+    tail -c +3 | head -c -1 | sed -z 's/\n\t[ *]/\x0/g' |
     { IFS= read -r -d ''; cat; } | grep -zi "$*" |
     choose -0uef -p "Select a line to run.")
   printf '%s' "$LINE" | choose_injector_wrapper.bash
