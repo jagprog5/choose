f480651b2eabcd7c20d086974abfce9298c521cd
John Giorshev <johngiorshev@gmail.com>
2023-05-01 12:34:32 -0400

    improve scripts

--- scripts/choose.bash
+++ scripts/choose.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
 hist() {
-  local LINE=$(
+  $(
     builtin fc -lnr -2147483648 |
-    tail -c +3 | head -c -1 | sed -z 's/\n\t[ *]/\x0/g' |
-    { IFS= read -r -d ''; cat; } | grep -zi "$*" |
-    choose -0ue --no-delimit --flip -p "Select an entry for input to the tty.")
-  printf '%s' "$LINE" | choose_injector_wrapper.bash
+      tail -c +3 | head -c -1 | sed -z 's/\n\t[ *]/\x0/g' |
+      {
+        IFS= read -r -d ''
+        cat
+      } | grep -zi -- "$*" |
+      choose -0ue --no-delimit --flip -p "Select an entry for input to the tty."
+  ) >choose_injector_wrapper.bash
 }
--- scripts/choose_injector_wrapper.bash
+++ scripts/choose_injector_wrapper.bash
@@ -2,10 +2,9 @@
 # modified from https://gist.github.com/johnlane/6bdeac9dd5a7b0c60820
 # this script is installed in /usr/bin and is used by choose.bash
 
-INPUT=$(</dev/stdin)
 saved_settings=$(stty -F /dev/tty -g)
 stty -F /dev/tty -echo -icanon min 1 time 0
-printf '%s' "$INPUT" | choose_injector
+choose_injector </dev/stdin
 until read -t0; do
   sleep 0.02
 done
